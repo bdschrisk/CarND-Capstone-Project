7878468f414c3e6bd5b377d7ac7a94ad92ea4c45
Carlos Arreaza <arreaza.c@gmail.com>
2017-10-04 22:28:04 -0400

    Add files via upload
    Added steering functionality

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -7,29 +7,27 @@ from geometry_msgs.msg import TwistStamped
 import math
 
 from twist_controller import Controller
+from yaw_controller import YawController
+from pid import PID
+from lowpass import LowPassFilter
 
 '''
 You can build this node only after you have built (or partially built) the `waypoint_updater` node.
-
 You will subscribe to `/twist_cmd` message which provides the proposed linear and angular velocities.
 You can subscribe to any other message that you find important or refer to the document for list
 of messages subscribed to by the reference implementation of this node.
-
 One thing to keep in mind while building this node and the `twist_controller` class is the status
 of `dbw_enabled`. While in the simulator, its enabled all the time, in the real car, that will
 not be the case. This may cause your PID controller to accumulate error because the car could
 temporarily be driven by a human instead of your controller.
-
 We have provided two launch files with this node. Vehicle specific values (like vehicle_mass,
 wheel_base) etc should not be altered in these files.
-
 We have also provided some reference implementations for PID controller and other utility classes.
 You are free to use them or build your own.
-
 Once you have the proposed throttle, brake, and steer values, publish it on the various publishers
 that we have created in the `__init__` function.
-
 '''
+
 
 class DBWNode(object):
     def __init__(self):
@@ -45,6 +43,7 @@ class DBWNode(object):
         steer_ratio = rospy.get_param('~steer_ratio', 14.8)
         max_lat_accel = rospy.get_param('~max_lat_accel', 3.)
         max_steer_angle = rospy.get_param('~max_steer_angle', 8.)
+        min_speed = rospy.get_param('~min_speed', 0.05)
 
         self.steer_pub = rospy.Publisher('/vehicle/steering_cmd',
                                          SteeringCmd, queue_size=1)
@@ -53,8 +52,11 @@ class DBWNode(object):
         self.brake_pub = rospy.Publisher('/vehicle/brake_cmd',
                                          BrakeCmd, queue_size=1)
 
+        self.yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
+        self.pid = PID(1, 0.005, 0.005)
+        self.low_pass_filter = LowPassFilter(0, 1)
         # TODO: Create `TwistController` object
-        self.controller = Controller()
+        self.controller = Controller(self.yaw_controller, self.pid, self.low_pass_filter)
 
         # TODO: Subscribe to all the topics you need to
         rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb)
@@ -68,19 +70,21 @@ class DBWNode(object):
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(50) # 50Hz
+        rate = rospy.Rate(50)  # 50Hz
         while not rospy.is_shutdown():
             # TODO: Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
-            throttle, brake, steering = self.controller.control(self.twist_cmd)
+            throttle, brake, steering = self.controller.control(self.twist_cmd.twist.linear.x, self.twist_cmd.twist.angular.z,
+                                                                self.current_velocity.twist.linear.x)
 
             if self.dbw_enabled:
-                self.publish(self.controller.throttle_cmd, 
-                    self.controller.brake_cmd, 
-                    self.controller.steer_cmd)
+                self.publish(throttle, brake, steering)
+            else:
+                #Reset PID when DBW is disabled, PID controls throttle
+                self.pid.reset()
 
             rate.sleep()
-    
+
     def dbw_enabled_cb(self, msg):
         self.dbw_enabled = msg.data
         # TODO: reset steering and throttle PID controllers accordingly
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,25 +1,30 @@
-
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, yaw_controller, pid, low_pass_filter):
+
+        self.yaw_controller = yaw_controller
+        self.pid = pid
+        self.low_pass_filter = low_pass_filter
+
         # TODO: Implement
         self.twist_cmd_reveived = None
         self.throttle_cmd = None
         self.brake_cmd = None
         self.steer_cmd = None
 
-
-    def control(self, *args, **kwargs):
+    def control(self, proposed_linear_vel, proposed_ang_vel, current_linear_vel):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
 
-        self.twist_cmd_received = args[0]
+        steering = self.yaw_controller.get_steering(proposed_linear_vel, proposed_ang_vel, current_linear_vel)
 
-        self.throttle_cmd = 0.75
+        #TODO: calculate throttle command and brake using PID maybe?
+
+        self.throttle_cmd = 0.2
         self.brake_cmd = 0.
-        self.steer_cmd = self.twist_cmd_received.twist.linear.x
+        self.steer_cmd = steering
 
         return self.throttle_cmd, self.brake_cmd, self.steer_cmd
