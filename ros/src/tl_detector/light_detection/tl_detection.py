5d9407a71eeb26728cbf4487b0a20a5ff6a1fc5b
fera0013 <ralphfehrer@gmail.com>
2017-10-10 07:53:01 +0200

    Switch to mobilenet

--- ros/src/tl_detector/light_detection/tl_detection.py
+++ ros/src/tl_detector/light_detection/tl_detection.py
@@ -34,7 +34,7 @@ def load_graph(graph_file):
 class TLDetection(object):
     def __init__(self):
         # load params
-        self.model_path= os.path.join(MODELS_DIR,'ssd_inception_v2_coco_11_06_2017','frozen_inference_graph.pb')
+        self.model_path= os.path.join(MODELS_DIR,'ssd_mobilenet_v1_coco_11_06_2017','frozen_inference_graph.pb')
         self.detection_graph,self.image_tensor, self.detection_boxes,self.detection_scores,self.detection_classes = load_graph(self.model_path)
 
     def to_image_coords(self,boxes, height, width):
